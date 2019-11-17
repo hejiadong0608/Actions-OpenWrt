9c5f91887718d230bfc121f2717c286a5f5cc3fe
hejiadong <hejiadong@vip.qq.com>
2019-11-17 12:48:47 +0800

    fix extra packages bug

--- diy.sh
+++ diy.sh
@@ -8,11 +8,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' openwrt/package/base-files/files/bin/config_generate
 
-git clone https://github.com/Lienol/openwrt-package
-mkdir packages
-mv openwrt-package/lienol/* packages
-rm -rf packages/shadowsocks-libev-new
-rm -rf packages/shadowsocksr-libev
-rm -rf packages/v2ray
 sed -i 's/bootstrap/material/g' openwrt/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/ built by Lean & KFERM//g' openwrt/package/lean/default-settings/files/zzz-default-settings
