123e4c2d860dc174313343d2ace8800311334c19
hejiadong <hejiadong@vip.qq.com>
2019-11-17 08:21:20 +0800

    update SSRPlus key

--- diy.sh
+++ diy.sh
@@ -8,12 +8,10 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' openwrt/package/base-files/files/bin/config_generate
 
-mkdir packages
-git clone https://github.com/Lienol/openwrt-package.git packages/lienol
-rm -rf packages/lienol/lienol/shadowsocks-libev-new
-rm -rf packages/lienol/lienol/shadowsocksr-libev
-rm -rf packages/lienol/lienol/v2ray
-mkdir -p files/etc/config
-echo  0xDEADBEEF > files/etc/config/google_fu_mode
+git clone https://github.com/Lienol/openwrt-package
+mv openwrt-package/lienol/* packages
+rm -rf packages/shadowsocks-libev-new
+rm -rf packages/shadowsocksr-libev
+rm -rf packages/v2ray
 sed -i 's/bootstrap/material/g' openwrt/package/lean/default-settings/files/zzz-default-settings
 sed -i 's/ built by Lean & KFERM//g' openwrt/package/lean/default-settings/files/zzz-default-settings
