bc231aadb2b573382b5500a011909b55661b1366
hejiadong <hejiadong@vip.qq.com>
2019-11-24 09:17:54 +0800

    fix diy script bug.

--- diy.sh
+++ diy.sh
@@ -8,8 +8,8 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-sed -i 's/bootstrap/material/g' openwrt/package/lean/default-settings/files/zzz-default-settings
-sed -i 's/ built by Lean & KFERM//g' openwrt/package/lean/default-settings/files/zzz-default-settings
-mkdir -p openwrt/files/etc/config
-echo  0xDEADBEEF > openwrt/files/etc/config/google_fu_mode
-rm -rf openwrt/package/lean/kcptun
+sed -i 's/bootstrap/material/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/ built by Lean & KFERM//g' package/lean/default-settings/files/zzz-default-settings
+mkdir -p files/etc/config
+echo  0xDEADBEEF > files/etc/config/google_fu_mode
+rm -rf package/lean/kcptun
