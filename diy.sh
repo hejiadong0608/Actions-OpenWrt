373b390e9e653352fde8e0abc00892fd55eb4dc9
hejiadong <hejiadong@vip.qq.com>
2019-12-08 19:32:07 +0800

    merge upstream sources from lean

--- diy.sh
+++ diy.sh
@@ -12,9 +12,6 @@ git clone https://github.com/Lienol/openwrt-package
 mv openwrt-package/lienol package/openwrt-packages
 rm -rf package/openwrt-packages/shadowsocks-libev-new
 rm -rf package/openwrt-packages/shadowsocksr-libev
-git config --global user.name "Your Name"
-git config --global user.email "you@example.com"
-./scripts/pull-leans
 
 sed -i 's/bootstrap/material/g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/ built by Lean & KFERM//g' package/lean/default-settings/files/zzz-default-settings
