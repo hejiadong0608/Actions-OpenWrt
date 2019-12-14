752ae54bb9475956e5cd869f24fad4c33a24ed79
hejiadong <hejiadong@vip.qq.com>
2019-12-14 08:55:56 +0800

    update config

--- diy.sh
+++ diy.sh
@@ -10,11 +10,19 @@
 
 git clone https://github.com/Lienol/openwrt-package
 mv openwrt-package/lienol package/openwrt-packages
+rm -rf package/openwrt-packages/v2ray
+rm -rf package/openwrt-packages/openssl1.1
+rm -rf package/openwrt-packages/trojan
 rm -rf package/openwrt-packages/shadowsocks-libev-new
 rm -rf package/openwrt-packages/shadowsocksr-libev
+rm -rf package/openwrt-packages/pdnsd-alt
+rm -rf package/lean/kcptun
+rm -rf package/lean/luci-app-kodexplorer
+rm -rf package/lean/luci-app-pppoe-relay
+rm -rf package/lean/luci-app-v2ray-server
 
 sed -i 's/bootstrap/material/g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/ built by Lean & KFERM//g' package/lean/default-settings/files/zzz-default-settings
 mkdir -p files/etc/config
 echo  0xDEADBEEF > files/etc/config/google_fu_mode
-sed -i 's/20190515/20191127/g' package/lean/kcptun/Makefile
+# sed -i 's/20190515/20191127/g' package/lean/kcptun/Makefile
