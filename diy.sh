63bcc7bd8668238a8c6bb9dd355025cf94605eab
hejiadong <hejiadong@vip.qq.com>
2020-02-23 19:11:06 +0800

    Update build config and diy script.

--- diy.sh
+++ diy.sh
@@ -13,15 +13,18 @@ mv openwrt-package/lienol package/openwrt-packages
 mv openwrt-package/package/* package/openwrt-packages
 mv openwrt-package/others/* package/openwrt-packages
 rm -rf package/openwrt-packages/v2ray
+rm -rf package/openwrt-packages/v2ray-plugin
 rm -rf package/openwrt-packages/openssl1.1
 rm -rf package/openwrt-packages/trojan
-rm -rf package/openwrt-packages/shadowsocks-libev-new
 rm -rf package/openwrt-packages/shadowsocksr-libev
 rm -rf package/openwrt-packages/pdnsd-alt
-rm -rf package/lean/kcptun
+rm -rf package/openwrt-packages/kcptun
+rm -rf package/openwrt-packages/simple-obfs
+rm -rf package/openwrt-packages/verysync
 rm -rf package/lean/luci-app-kodexplorer
+rm -rf package/openwrt-packages/ipt2socks
 rm -rf package/lean/luci-app-pppoe-relay
-rm -rf package/lean/luci-app-v2ray-server
+rm -rf package/openwrt-packages/luci-app-v2ray-server
 
 sed -i 's/bootstrap/material/g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/ built by Lean & KFERM//g' package/lean/default-settings/files/zzz-default-settings
