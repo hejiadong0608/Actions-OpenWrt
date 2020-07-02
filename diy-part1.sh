36c03dec70958ad8151b9110d416da7efbe3878c
hejiadong <hejiadong@vip.qq.com>
2020-07-02 15:32:18 +0800

    Update

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,25 +12,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-mkdir package/openwrt-packages
-tar -xzf $GITHUB_WORKSPACE/luci-app-passwall.tgz
-mv luci-app-passwall package/openwrt-packages
-git clone https://github.com/Lienol/openwrt-package
-mv openwrt-package/lienol/* package/openwrt-packages
-mv openwrt-package/package/* package/openwrt-packages
-mv openwrt-package/others/* package/openwrt-packages
-rm -rf package/openwrt-packages/v2ray
-rm -rf package/openwrt-packages/v2ray-plugin
-rm -rf package/openwrt-packages/trojan
-rm -rf package/openwrt-packages/shadowsocksr-libev
-rm -rf package/openwrt-packages/pdnsd-alt
-rm -rf package/openwrt-packages/kcptun
-rm -rf package/openwrt-packages/simple-obfs
-rm -rf package/openwrt-packages/verysync
-rm -rf package/lean/luci-app-kodexplorer
-rm -rf package/lean/ipt2socks
-rm -rf package/lean/luci-app-pppoe-relay
-rm -rf package/lean/luci-app-v2ray-server
-rm -rf package/openwrt-packages/luci-app-verysync
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
