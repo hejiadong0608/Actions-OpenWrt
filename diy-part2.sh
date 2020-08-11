8756c9ba37d1e4ad0c001204a6368ee3a5548efe
hejiadong <hejiadong@vip.qq.com>
2020-08-11 11:23:49 +0800

    add xiaomi r3pro workflows/hc5962.yml

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,5 +14,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 sed -i 's/PATCHVER:=5.4/PATCHVER:=4.19/g' target/linux/x86/Makefile
+sed -i 's/bootstrap/material/g' package/lean/default-settings/files/zzz-default-settings
 mkdir -p files/etc/config
 echo  0xDEADBEEF > files/etc/config/google_fu_mode
