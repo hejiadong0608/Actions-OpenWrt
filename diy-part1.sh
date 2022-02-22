ee0a67ba4c050f912c1bb57a3ee0c9fa2363ccf1
hjd <hejiadong@vip.qq.com>
2022-02-22 14:10:53 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,8 +13,6 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-rm -rf package/lean/luci-lib-docker
-
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
@@ -24,5 +22,5 @@ rm -rf package/lean/luci-lib-docker
 #sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-git clone https://github.com/KFERMercer/luci-app-tcpdump.git ./package/luci-app-tcpdump
+git clone https://github.com/KFERMercer/luci-app-tcpdump.git ./package/lean/luci-app-tcpdump
 #echo ' src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
