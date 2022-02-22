9066c1478d56b06f33662e6ad4c8907215e66872
hjd <hejiadong@vip.qq.com>
2022-02-22 13:41:33 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,11 +19,10 @@ rm -rf package/lean/luci-lib-docker
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
-sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
+#sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-git clone https://github.com/lisaac/luci-app-dockerman ./package/luci-app-dockerman
-git clone https://github.com/lisaac/luci-lib-docker ./package/lean/luci-lib-docker
 git clone https://github.com/KFERMercer/luci-app-tcpdump.git ./package/luci-app-tcpdump
+#echo ' src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
