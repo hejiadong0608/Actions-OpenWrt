12bf4407505ffc1352bee6738064571dc108f26d
hjd <hejiadong@vip.qq.com>
2022-04-19 10:41:36 +0800

    add Redmi AX6S

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,5 +23,4 @@
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-git clone https://github.com/KFERMercer/luci-app-tcpdump.git ./package/lean/luci-app-tcpdump
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
