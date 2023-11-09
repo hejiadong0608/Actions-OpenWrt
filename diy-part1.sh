f43a3b1a093e93166ccb16d944c949ed03b86119
hjd <hejiadong@vip.qq.com>
2023-11-09 18:57:05 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,3 +24,4 @@
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+git clone --depth=1 -b main https://github.com/fw876/helloworld.git package/helloworld
