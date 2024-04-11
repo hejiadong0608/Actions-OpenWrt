3812881e1cfa1611a8523b2ff46e94ce2fe587f7
hjd <hejiadong@vip.qq.com>
2024-04-11 17:26:13 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,5 +24,5 @@
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git passwall-packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
