0c4829576df7ca0bdedfbf9ecb6804438fbcff5e
hjd <hejiadong@vip.qq.com>
2024-04-13 13:36:49 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,7 +23,7 @@
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 echo 'src-git airconnect https://github.com/sbwml/luci-app-airconnect' >>feeds.conf.default
