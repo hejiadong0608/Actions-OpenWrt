5fc0314e8249cf91da9011f0dc04a42cf0cd4dbd
hjd <hejiadong@vip.qq.com>
2024-04-15 14:04:58 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,6 +23,7 @@
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
+git clone https://github.com/hejiadong0608/luci-app-msd_lite.git package/luci-app-msd_lite
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
