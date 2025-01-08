3da0b23514aba80270c7ef23c5e1d31b1f6a38fb
hjd <hejiadong@vip.qq.com>
2025-01-08 19:39:17 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,8 @@ sed -i 's/bootstrap/material/g' package/lean/default-settings/files/zzz-default-
 sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd    # 替换终端为bash
 
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb,tailscale}
-rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
 rm -rf feeds/packages/net/tailscale
 git clone https://github.com/openwrt/packages
 mv packages/net/tailscale feeds/packages/net
