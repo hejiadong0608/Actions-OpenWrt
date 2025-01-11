d75b859c63eee04b33a431890ca7283d7eab504f
hjd <hejiadong@vip.qq.com>
2025-01-11 09:44:06 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,12 +9,8 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb,tailscale}
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+rm -rf feeds/packages/tailscale
+rm -rf feeds/luci/applications/{luci-app-passwall*,luci-app-tailscale}
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall-packages
-git clone https://github.com/openwrt/packages
-mv packages/net/tailscale package/lean
-sed -i "s/'nftables'/'iptables'/" package/lean/tailscale/files/tailscale.conf
-rm -rf packages
 ./scripts/feeds update -ai
